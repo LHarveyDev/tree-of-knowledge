a1164e688c2dd6f5846146ae1f0e4ce39fb3bdf7
LHarveyDev <>
2023-10-15 10:40:28 +0000

    Add image and code credits

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,7 +59,6 @@ const questionElement = document.getElementById("question");
 const answerButtons = document.getElementById("answer-buttons");
 const nextButton = document.getElementById("next-btn");
 
-
 let currentQuestionIndex = 0;
 let score = 0;
 
@@ -139,5 +138,3 @@ nextButton.addEventListener("click", () => {
 });
 
 startQuiz();
-
-
